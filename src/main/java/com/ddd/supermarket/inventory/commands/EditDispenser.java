c6e9e38e9da3f600cea28a5a7d02817191a81f3e
Juan Vargas <juandeveloper19@gmail.com>
2022-05-22 07:14:44 -0500

    Command to edit dispensers

--- src/main/java/com/ddd/supermarket/inventory/commands/EditDispenser.java
+++ src/main/java/com/ddd/supermarket/inventory/commands/EditDispenser.java
@@ -1,2 +1,38 @@
-package com.ddd.supermarket.inventory.commands;public class EditDispenser {
+package com.ddd.supermarket.inventory.commands;
+
+import co.com.sofka.domain.generic.Command;
+
+import com.ddd.supermarket.inventory.value.objects.InventoryID;
+import com.ddd.supermarket.inventory.value.objects.DispenserID;
+import com.ddd.supermarket.inventory.value.objects.Name;
+import com.ddd.supermarket.inventory.value.objects.Phone;
+
+public class EditDispenser extends Command {
+    private final InventoryID inventoryID;
+    private final DispenserID dispenserID;
+    private final Name name;
+    private final Phone phone;
+
+    public EditDispenser(InventoryID inventoryID, DispenserID dispenserID, Name name, Phone phone) {
+        this.inventoryID = inventoryID;
+        this.dispenserID = dispenserID;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public InventoryID getInventoryID() {
+        return inventoryID;
+    }
+
+    public DispenserID getDispenserID() {
+        return dispenserID;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
 }
